7f565f79c0c494cfea2006d69c22c416b13d2fd1
ggracel <gasper.mcc@gmail.com>
2026-09-19 19:49:28 +0200

    Add files via upload

--- uraplus/sw.js
+++ uraplus/sw.js
@@ -1,5 +1,5 @@
 /* Ura+ service worker — offline podpora */
-var CACHE = 'uraplus-v8';
+var CACHE = 'uraplus-v9';
 var ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
